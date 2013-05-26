b5b51ea91d3373de3c6d750e8a74b4ec8be86d72
Iñaki Abete <inakiabt@gmail.com>
2013-05-26 00:27:58 -0300

    custom

---
+++ productgram/recipes/symfony.rb
@@ -0,0 +1,19 @@
+node[:deploy].each do |app_name, deploy|
+
+  template "/etc/php.d/timezone.ini" do
+      source "timezone.erb"
+      owner "root"
+      mode "0755"
+  end
+
+  script "set_cache_log_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+      sudo setfacl -R -m u:#{deploy[:user]}:rwX -m u:#{deploy[:group]}:rwX app/cache app/logs
+      sudo setfacl -dR -m u:#{deploy[:user]}:rwx -m u:#{deploy[:group]}:rwx app/cache app/logs
+    EOH
+  end
+
+end
