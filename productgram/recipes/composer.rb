24f7799eadf0ed1eedd9ea0cdbe7d3f6df9e8aec
Iñaki Abete <inakiabt@gmail.com>
2013-05-26 01:35:58 -0300

    test

--- productgram/recipes/composer.rb
+++ productgram/recipes/composer.rb
@@ -3,7 +3,7 @@ node[:deploy].each do |app_name, deploy|
   script "install_composer" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current"
+    cwd "#{deploy[:deploy_to]}/current/Symfony"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
     php composer.phar install
