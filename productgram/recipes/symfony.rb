d52b5c3707a5f094ecac9405c958d9ef71099d8b
Iñaki Abete <inakiabt@gmail.com>
2013-05-26 02:00:55 -0300

    test

--- productgram/recipes/symfony.rb
+++ productgram/recipes/symfony.rb
@@ -11,6 +11,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
+      sudo mkdir -p Symfony/app/{cache,logs}
       sudo setfacl -R -m u:#{deploy[:user]}:rwX -m u:#{deploy[:group]}:rwX Symfony/app/cache Symfony/app/logs
       sudo setfacl -dR -m u:#{deploy[:user]}:rwx -m u:#{deploy[:group]}:rwx Symfony/app/cache Symfony/app/logs
     EOH
