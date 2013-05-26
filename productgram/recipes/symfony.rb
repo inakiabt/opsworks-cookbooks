b87a19d11aaec7e6159e6279fd12fc1f2896902f
Iñaki Abete <inakiabt@gmail.com>
2013-05-26 01:55:13 -0300

    test

--- productgram/recipes/symfony.rb
+++ productgram/recipes/symfony.rb
@@ -1,16 +1,9 @@
-Chef::Log.debug("Starting...")
+node[:deploy].each do |application, deploy|
+
   template "/etc/php.d/timezone.ini" do
       source "timezone.erb"
       owner "root"
       mode "0755"
-  end
-
-
-node[:deploy].each do |application, deploy|
-
-Chef::Log.debug("Entra viejo?")
-  execute "test_touch" do
-    command "touch /tmp/holamundo.txt"
   end
 
   script "set_cache_log_permissions" do
@@ -18,8 +11,8 @@ Chef::Log.debug("Entra viejo?")
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-      sudo setfacl -R -m u:#{deploy[:user]}:rwX -m u:#{deploy[:group]}:rwX Symfony/app/cache app/logs
-      sudo setfacl -dR -m u:#{deploy[:user]}:rwx -m u:#{deploy[:group]}:rwx Symfony/app/cache app/logs
+      sudo setfacl -R -m u:#{deploy[:user]}:rwX -m u:#{deploy[:group]}:rwX Symfony/app/cache Symfony/app/logs
+      sudo setfacl -dR -m u:#{deploy[:user]}:rwx -m u:#{deploy[:group]}:rwx Symfony/app/cache Symfony/app/logs
     EOH
   end
 
