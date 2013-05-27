d816379113d9a437ec02e740bacb0a436086085a
Iñaki Abete <inakiabt@gmail.com>
2013-05-27 11:20:44 -0300

    aws elb ping file

--- productgram/recipes/awselb.rb
+++ productgram/recipes/awselb.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
 
-  template "#{deploy[:deploy_to]}/Symfony/web/ping.html" do
+  template "#{deploy[:deploy_to]}/current/Symfony/web/ping.html" do
       source "ping.erb"
       owner deploy[:user]
       mode "0755"
