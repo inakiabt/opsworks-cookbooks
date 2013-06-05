75cbb25453800c3e980eab0e47c5b2279fdd7d66
Iñaki Abete <inakiabt@gmail.com>
2013-06-05 14:34:13 -0300

    Added composer official cookbook

--- productgram/recipes/composer.rb
+++ productgram/recipes/composer.rb
@@ -1,13 +1,23 @@
+include_recipe "composer"
+
 node[:deploy].each do |app_name, deploy|
 
-  script "install_composer" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current/Symfony"
-    code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install
-    EOH
-  end
+	composer "/usr/local/bin" do
+	  owner "root" # optional
+	  action [:install]
+	end
+
+	composer_project "#{deploy[:deploy_to]}/current/Symfony" do
+	 action [:update]
+	end
+  # script "install_composer" do
+  #   interpreter "bash"
+  #   user "root"
+  #   cwd "#{deploy[:deploy_to]}/current/Symfony"
+  #   code <<-EOH
+  #   curl -s https://getcomposer.org/installer | php
+  #   php composer.phar install
+  #   EOH
+  # end
 
 end
