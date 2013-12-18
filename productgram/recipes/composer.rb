0c74877c5d4045159edcf4fa22a1b37e964b05e8
Iñaki Abete <inakiabt@gmail.com>
2013-12-18 14:34:12 -0300

    Added new symfony cookbook

--- productgram/recipes/composer.rb
+++ productgram/recipes/composer.rb
@@ -13,14 +13,5 @@ node[:deploy].each do |app_name, deploy|
 		quiet false
 	 	action [:install, :update]
 	end
-  # script "install_composer" do
-  #   interpreter "bash"
-  #   user "root"
-  #   cwd "#{deploy[:deploy_to]}/current/Symfony"
-  #   code <<-EOH
-  #   curl -s https://getcomposer.org/installer | php
-  #   php composer.phar install
-  #   EOH
-  # end
 
 end
--- productgram/recipes/symfony.rb
+++ productgram/recipes/symfony.rb
@@ -1,3 +1,5 @@
+include_recipe "symfony2"
+
 node[:deploy].each do |application, deploy|
 
   template "/etc/php.d/timezone.ini" do
@@ -6,31 +8,63 @@ node[:deploy].each do |application, deploy|
       mode "0755"
   end
 
-  script "set_cache_log_permissions" do
-    interpreter "bash"
-    user "#{deploy[:user]}"
-    group "#{deploy[:group]}"
-    cwd "#{deploy[:deploy_to]}/current/Symfony"
-    code <<-EOH
-      php app/console assets:install --env=prod
-      php app/console assetic:dump --env=prod
-    EOH
-#      sudo setfacl -R -m u:#{deploy[:group]}:rwX -m u:#{deploy[:user]}:rwX app/cache app/logs
-#      sudo setfacl -dR -m u:#{deploy[:group]}:rwx -m u:#{deploy[:user]}:rwx app/cache app/logs
+  path = "#{deploy[:deploy_to]}/current/Symfony"
+  # Deletes hard copied bundles
+  directory "#{path}/web/bundles" do
+    recursive true
+    action :delete
   end
 
-  directory "#{deploy[:deploy_to]}/current/Symfony/app/cache" do
-    owner "#{deploy[:user]}"
-    group "#{deploy[:group]}"
-    recursive true
-    mode "777"
+  symfony2_console "assets:install" do
+    action :cmd
+    command "assets:install --symlink"
+    path path
   end
 
-  directory "#{deploy[:deploy_to]}/current/Symfony/app/logs" do
-    owner "#{deploy[:user]}"
-    group "#{deploy[:group]}"
-    recursive true
-    mode "777"
+  symfony2_console "assetic:dump" do
+    action :cmd
+    command "assetic:dump"
+    path path
   end
+
+  symfony2_console "cache:clear" do
+    action :cmd
+    command "cache:clear"
+    path path
+  end
+
+  symfony2_console "cache:warmup" do
+    action :cmd
+    command "cache:warmup"
+    path path
+  end
+
+
+#   script "set_cache_log_permissions" do
+#     interpreter "bash"
+#     user "#{deploy[:user]}"
+#     group "#{deploy[:group]}"
+#     cwd "#{deploy[:deploy_to]}/current/Symfony"
+#     code <<-EOH
+#       php app/console assets:install --env=prod
+#       php app/console assetic:dump --env=prod
+#     EOH
+# #      sudo setfacl -R -m u:#{deploy[:group]}:rwX -m u:#{deploy[:user]}:rwX app/cache app/logs
+# #      sudo setfacl -dR -m u:#{deploy[:group]}:rwx -m u:#{deploy[:user]}:rwx app/cache app/logs
+#   end
+
+#   directory "#{deploy[:deploy_to]}/current/Symfony/app/cache" do
+#     owner "#{deploy[:user]}"
+#     group "#{deploy[:group]}"
+#     recursive true
+#     mode "777"
+#   end
+
+#   directory "#{deploy[:deploy_to]}/current/Symfony/app/logs" do
+#     owner "#{deploy[:user]}"
+#     group "#{deploy[:group]}"
+#     recursive true
+#     mode "777"
+#   end
 
 end
