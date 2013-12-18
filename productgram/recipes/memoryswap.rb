cf482244f8d7db2c2e2078c9f213cfa939244875
Iñaki Abete <inakiabt@gmail.com>
2013-12-18 16:00:18 -0300

    Add creation action to ensure cache/log dirs exists

---
+++ productgram/recipes/memoryswap.rb
@@ -0,0 +1,14 @@
+# Source: https://github.com/onema/opsworks-chef-cookbooks/blob/master/phpenv/recipes/memoryswap.rb
+# Add memory swap: This should be used in instances with low RAM to prevent
+# "out of memory" issues.
+#
+script "memory_swap" do
+  interpreter "bash"
+  user "root"
+  cwd "/"
+  code <<-EOH
+  /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
+  /sbin/mkswap /var/swap.1
+  /sbin/swapon /var/swap.1
+  EOH
+end
--- productgram/recipes/symfony.rb
+++ productgram/recipes/symfony.rb
@@ -60,15 +60,17 @@ node[:deploy].each do |application, deploy|
   directory "#{deploy[:deploy_to]}/current/Symfony/app/cache" do
     owner "#{deploy[:user]}"
     group "#{deploy[:group]}"
-    recursive true
     mode "777"
+    recursive true
+    action :create
   end
 
   directory "#{deploy[:deploy_to]}/current/Symfony/app/logs" do
     owner "#{deploy[:user]}"
     group "#{deploy[:group]}"
-    recursive true
     mode "777"
+    recursive true
+    action :create
   end
 
 end
